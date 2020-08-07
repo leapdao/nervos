c32898247f08f334a990e6ad0071879dff69ac7b
Keno Budde <Clearwood@users.noreply.github.com>
2020-08-07 11:26:10 +0200

    uncomplete tests

--- packages/childchain/contracts/bridge.sol
+++ packages/childchain/contracts/bridge.sol
@@ -1,18 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.21 <0.7.0;
+pragma experimental ABIEncoderV2;
 
 contract Bridge {
 
   event LockSig(bytes32 indexed txHash, address indexed validator, address to, uint256 amount);
+  event Burn(address indexed sender, uint256 value);
+  event UnlockSig(bytes32 indexed txHash, address indexed validator, address from, uint256 amount);
+  event BurnQuorum(bytes32 indexed txHash, address indexed from, uint256 amount, Sig[] signatures);
 
   address[] public validators;
   mapping(bytes32 => mapping(address => bool)) lockSigs;
   // mapping(bytes32 => address[]) lockSigs;
 
+  struct Sig {
+    bool complete;
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+  }
+
+  mapping(bytes32 => mapping(address => Sig)) unlockSigs;
+
   constructor(address[] memory _validators) public {
     validators = _validators;
   }
 
+  function () payable external {
+      for (uint256 v = 0; v < validators.length; v++) {
+        if (validators[v] == msg.sender) {
+          return;
+        }
+      }
+      emit Burn(msg.sender, msg.value);
+    }
+
+    function collectUnlock(
+      address from,
+      uint256 amount,
+      bytes32 txHash,
+      uint8 v,
+      bytes32 r,
+      bytes32 s) public {
+      // check the unlock
+      require(!unlockSigs[txHash][from].complete, "burn already completed");
+      bytes32 sigHash = keccak256(abi.encode(from, amount, txHash));
+      address signer;
+      (, signer) = safer_ecrecover(sigHash, v, r, s);
+      uint256 signerCount = 0;
+      for (uint256 i = 0; i < validators.length; i++) {
+        if (validators[i] == signer) {
+          // payload
+          unlockSigs[txHash][signer] = Sig({
+            complete: true,
+            v: v,
+            r: r,
+            s: s
+          });
+          emit UnlockSig(txHash, signer, from, amount);
+        }
+        if (unlockSigs[txHash][validators[i]].v > 0) {
+          signerCount++;
+        }
+      }
+      if (signerCount >= validators.length * 2 / 3) {
+        // how to mint?! :shrug:
+        // set the lock
+        Sig[] memory signatures  = new Sig[](validators.length * 2 / 3);
+        uint256 fillUntil = 0;
+        for (uint256 i = 0; i < validators.length; i++){
+          if (unlockSigs[txHash][validators[i]].v > 0) {
+            signatures[fillUntil] = unlockSigs[txHash][validators[i]];
+            fillUntil++;
+          }
+        }
+        emit BurnQuorum(txHash, from, amount, signatures);
+        unlockSigs[txHash][from].complete = true;
+      }
+      require(signerCount > 0, "Signer needs to be part of validator set");
+    }
 
   function safer_ecrecover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal returns (bool, address) {
     // We do our own memory management here. Solidity uses memory offset
@@ -60,15 +126,16 @@ contract Bridge {
         lockSigs[txHash][signer] = true;
         emit LockSig(txHash, signer, to, amount);
       }
-      if (lockSigs[txHash][signer]) {
+      if (lockSigs[txHash][validators[v]]) {
         signerCount++;
       }
     }
     if (signerCount > validators.length * 2 / 3) {
       // how to mint?! :shrug:
-      //to.transfer(amount);
+      to.transfer(amount);
       // set the lock
       lockSigs[txHash][to] = true;
     }
+    require(signerCount > 0, "Signer needs to be part of validator set");
   }
 }
--- packages/childchain/test/bridge.js
+++ packages/childchain/test/bridge.js
@@ -1,28 +1,148 @@
 const Bridge = artifacts.require("Bridge");
-const ethUtil = require('ethereumjs-util');
+const ethUtil = require("ethereumjs-util");
+var abi = require("ethereumjs-abi");
 
-contract('Bridge', accounts => {
-	const alice = '0xf3beac30c498d9e26865f34fcaa57dbb935b0d74';
-	const txHash = '0x1122334411223344112233441122334411223344112233441122334411223344';
-	const validatorPriv = '0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f';
-	let bridge;
+contract("Bridge", (accounts) => {
+  const alice = "0xf3beac30c498d9e26865f34fcaa57dbb935b0d74";
+  const txHash =
+    "0x1122334411223344112233441122334411223344112233441122334411223344";
+  const validatorPriv =
+    "0x278a5de700e29faae8e40e366ec5012b5ec63d36ec77e8a2417154cc1d25383f";
+  let bridge;
+  const bob = "0xe10f3d125e5f4c753a6456fc37123cf17c6900f2";
+  const bobPriv =
+    "0x7bc8feb5e1ce2927480de19d8bc1dc6874678c016ae53a2eec6a6e9df717bfac";
 
-	before(async() => {
-		bridge = await Bridge.new([alice]);
-	});
+  beforeEach(async () => {
+    bridge = await Bridge.new([alice]);
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: bridge.address,
+      value: "2000000000000000000",
+    });
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: bob,
+      value: "2000000000000000000",
+    });
+  });
 
-	it('should allow to collect 1', async() => {
+  it("should allow and collect unlock signatures", async () => {
+    let bobAccount = await web3.eth.accounts.privateKeyToAccount(bobPriv);
+    let txHash = await web3.eth.sendTransaction({
+      from: bob,
+      to: bridge.address,
+      value: "1000000000000000000",
+    });
+    let burnEvents = await bridge.Burn(
+      { sender: bob },
+      { fromBlock: 0, toBlock: "latest" }
+    );
+    let txHash = burnEvents[0].transactionHash;
+    let payload = abi.rawEncode(
+      ["address", "uint256", "bytes32"],
+      [alice, "1000000000000000000", txHash]
+    );
+    const sigHash = ethUtil.keccak256(payload);
+    const sig = ethUtil.ecsign(
+      sigHash,
+      Buffer.from(validatorPriv.replace("0x", ""), "hex")
+    );
+    const preAmount = await web3.eth.getBalance(alice);
+    await bridge.collectUnlock(
+      alice,
+      "1000000000000000000",
+      txHash,
+      sig.v,
+      sig.r,
+      sig.s
+    );
+    const postAmount = await web3.eth.getBalance(alice);
+  });
 
-		// generate sig
-		const buf = Buffer.alloc(96);
-		Buffer.from(alice.replace('0x', ''), 'hex').copy(buf, 0, 0, 20);
-		Buffer.from('0000000000000000000000000000000000000000000000000DE0B6B3A7640000', 'hex').copy(buf, 32, 0, 32);
-		Buffer.from(txHash.replace('0x', ''), 'hex').copy(buf, 64, 0, 32);
-		const sigHash = ethUtil.keccak256(buf);
-		console.log(sigHash);
-    const sig = ethUtil.ecsign(Buffer.from(validatorPriv.replace('0x', ''), 'hex'), sigHash);
-    console.log(sig);
-		await bridge.collectLock(alice, '1000000000000000000', txHash, sig.v, sig.r, sig.s);
+  it("should allow to collect 1 and transfer to Alice", async () => {
+    // generate sig
+    let payload = abi.rawEncode(
+      ["address", "uint256", "bytes32"],
+      [alice, "1000000000000000000", txHash]
+    );
+    const sigHash = ethUtil.keccak256(payload);
+    const sig = ethUtil.ecsign(
+      sigHash,
+      Buffer.from(validatorPriv.replace("0x", ""), "hex")
+    );
+    const preAmount = await web3.eth.getBalance(alice);
+    await bridge.collectLock(
+      alice,
+      "1000000000000000000",
+      txHash,
+      sig.v,
+      sig.r,
+      sig.s
+    );
+    const postAmount = await web3.eth.getBalance(alice);
+    assert.equal(postAmount - 1000000000000000000, preAmount);
+  });
 
-	});
-})
+  it("should not allow Bob as validator.", async () => {
+    // generate sig
+    let payload = abi.rawEncode(
+      ["address", "uint256", "bytes32"],
+      [bob, "1000000000000000000", txHash]
+    );
+    const sigHash = ethUtil.keccak256(payload);
+    const sig = ethUtil.ecsign(
+      sigHash,
+      Buffer.from(bobPriv.replace("0x", ""), "hex")
+    );
+    try {
+      await bridge.collectLock(
+        bob,
+        "1000000000000000000",
+        txHash,
+        sig.v,
+        sig.r,
+        sig.s
+      );
+      throw new Error("expected to throw");
+    } catch (error) {
+      assert(
+        error.message.includes("Signer needs to be part of validator set")
+      );
+    }
+  });
+
+  it("should not allow submit on executed relay.", async () => {
+    // generate sig
+    let payload = abi.rawEncode(
+      ["address", "uint256", "bytes32"],
+      [alice, "1000000000000000000", txHash]
+    );
+    const sigHash = ethUtil.keccak256(payload);
+    const sig = ethUtil.ecsign(
+      sigHash,
+      Buffer.from(validatorPriv.replace("0x", ""), "hex")
+    );
+    await bridge.collectLock(
+      alice,
+      "1000000000000000000",
+      txHash,
+      sig.v,
+      sig.r,
+      sig.s
+    );
+    try {
+      await bridge.collectLock(
+        alice,
+        "1000000000000000000",
+        txHash,
+        sig.v,
+        sig.r,
+        sig.s
+      );
+      throw new Error("expected to throw");
+    } catch (error) {
+      assert(error.message.includes("mint already executed"));
+    }
+  });
+});
