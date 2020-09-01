37c5bb43d7f96107d83c2821178dc9c908fa5a3c
Jan <jan@parseclabs.org>
2020-09-01 16:25:50 +0200

    Add deploy state transition.

--- packages/parent-bridge/contracts/parent-bridge/src/main.rs
+++ packages/parent-bridge/contracts/parent-bridge/src/main.rs
@@ -9,20 +9,18 @@ use core::result::Result;
 
 // Import heap related library from `alloc`
 // https://doc.rust-lang.org/alloc/index.html
-use alloc::{vec, vec::Vec};
-
+use alloc::{vec::Vec};
 
 use ckb_std::{
-    entry,
-    default_alloc,
-    debug,
-    high_level::{load_script, load_tx_hash},
+    ckb_constants::Source,
+    ckb_types::{bytes::Bytes},
+    debug, default_alloc, entry,
     error::SysError,
-    ckb_types::{bytes::Bytes, prelude::*},
+    high_level::{
+        load_cell_data, load_cell_lock, load_cell_type_hash, load_script, load_script_hash,
+            },
+    syscalls::load_witness,
 };
-
-use sha3::{Digest, Keccak256};
-use hex::encode;
 
 entry!(entry);
 default_alloc!();
@@ -36,13 +34,31 @@ fn entry() -> i8 {
     }
 }
 
+// FROM rfc
+// const ANYONE_CAN_PAY_CODE_HASH : [u8; 32] = [
+//     0x86, 0xa1, 0xc6, 0x98, 0x7a, 0x4a, 0xcb, 0xe1, 0xa8, 0x87, 0xcc, 0xa4, 0xc9, 0xdd,
+//     0x2a, 0xc9, 0xfc, 0xb0, 0x74, 0x05, 0xbb, 0xed, 0xa5, 0x1b, 0x86, 0x1b, 0x18, 0xbb,
+//     0xf7, 0x49, 0x2c, 0x4b
+// ];
+
+// FROM tests
+const ANYONE_CAN_PAY_CODE_HASH: [u8; 32] = [
+    230, 131, 176, 65, 57, 52, 71, 104, 52, 132, 153, 194, 62, 177, 50, 109, 90, 82, 214, 219, 0,
+    108, 13, 47, 236, 224, 10, 131, 31, 54, 96, 215,
+];
+
 /// Error
 #[repr(i8)]
 enum Error {
     IndexOutOfBound = 1,
-    ItemMissing,
-    LengthNotEnough,
-    Encoding,
+    ItemMissing = 2,
+    LengthNotEnough = 3,
+    Encoding = 4,
+    StateTransitionDoesNotExist = 5,
+    WrongValidatorListLength = 6,
+    WrongLockScript = 7,
+    WrongTypeScript = 8,
+    DataLengthNotZero = 9,
     // Add customized errors here...
 }
 
@@ -54,27 +70,87 @@ impl From<SysError> for Error {
             ItemMissing => Self::ItemMissing,
             LengthNotEnough(_) => Self::LengthNotEnough,
             Encoding => Self::Encoding,
+            // StateTransitionDoesNotExist => Self::StateTransitionDoesNotExist,
+            // WrongValidatorListLength => Self::WrongValidatorListLength,
+            // WrongLockScript => Self::WrongLockScript,
+            // WrongTypeScript => Self::WrongTypeScript,
+            // DataLengthNotZero => Self::DataLengthNotZero,
             Unknown(err_code) => panic!("unexpected sys error {}", err_code),
         }
     }
 }
 
-const DEPOSIT_LOCK_SCRIPT = 3;
+type Address = [u8; 20];
+
+enum StateTransition {
+    DeployBridge { validators: Vec<Address> },
+}
+
+impl StateTransition {
+    fn verify(&self) -> Result<(), Error> {
+        match self {
+            Self::DeployBridge { validators: _ } => {
+                // lock script on output0 should be anyone can spend
+                let lock_code_hash = load_cell_lock(0, Source::Output)?.code_hash().raw_data();
+                if *lock_code_hash != ANYONE_CAN_PAY_CODE_HASH[..] {
+                    return Err(Error::WrongLockScript);
+                }
+                // type script on output0 should be our script
+                let type_script_hash = load_cell_type_hash(0, Source::Output)?.unwrap();
+                let script_hash = load_script_hash()?;
+
+                if type_script_hash != script_hash {
+                    return Err(Error::WrongTypeScript);
+                }
+                // data on output0 should be nothing
+                let data = load_cell_data(0, Source::Output)?;
+
+                if !data.len() == 0 {
+                    return Err(Error::DataLengthNotZero);
+                }
+
+                Ok(())
+            }
+        }
+    }
+}
+
+fn slice_to_array_20(slice: &[u8]) -> [u8; 20] {
+    let mut array = [0u8; 20];
+    for (&x, p) in slice.iter().zip(array.iter_mut()) {
+        *p = x;
+    }
+    array
+}
+
+fn parse_validator_list_from_args(args: &[u8]) -> Result<Vec<Address>, Error> {
+    if args.len() % 20 != 0 {
+        return Err(Error::WrongValidatorListLength);
+    }
+    let mut validators = Vec::new();
+    for i in 0..(args.len() / 20) {
+        validators.push(slice_to_array_20(&args[i..i + 20]));
+    }
+    Ok(validators)
+}
+
+fn get_state_transition() -> Result<StateTransition, Error> {
+    let mut wit_buf: [u8; 1] = [0; 1];
+    load_witness(&mut wit_buf, 0, 0, Source::Input)?;
+    match wit_buf[0] {
+        0 => {
+            let script_args: Bytes = load_script()?.args().raw_data();
+            let validators = parse_validator_list_from_args(&*script_args)?;
+            debug!("validators: {:?}", validators);
+            Ok(StateTransition::DeployBridge {
+                validators: validators,
+            })
+        }
+        _ => Err(Error::StateTransitionDoesNotExist),
+    }
+}
 
 fn main() -> Result<(), Error> {
-    // remove below examples and write your code here
-
-    let script = load_script()?;
-    let args: Bytes = script.args().unpack();
-    debug!("script args is {:?}", args);
-
-    let tx_hash = load_tx_hash()?;
-    debug!("tx hash is {:?}", tx_hash);
-
-    let hash = Keccak256::digest(&*args);
-    debug!("Hash: {:?}", encode(hash));
-
-    let _buf: Vec<_> = vec![0u8; 32];
-
-    Ok(())
+    let state_transition = get_state_transition()?;
+    state_transition.verify()
 }
--- packages/parent-bridge/tests/src/tests.rs
+++ packages/parent-bridge/tests/src/tests.rs
@@ -1,5 +1,5 @@
 use super::*;
-use ckb_testtool::context::Context;
+use ckb_testtool::{builtin::ALWAYS_SUCCESS, context::Context};
 use ckb_tool::ckb_types::{
     bytes::Bytes,
     core::TransactionBuilder,
@@ -13,43 +13,48 @@ const MAX_CYCLES: u64 = 10_000_000;
 
 #[test]
 fn test_basic() {
-    // deploy contract
     let mut context = Context::default();
-    let contract_bin: Bytes = Loader::default().load_binary("parent-bridge");
-    let out_point = context.deploy_cell(contract_bin);
 
-    // prepare scripts
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
     let lock_script = context
-        .build_script(&out_point, Bytes::from(Vec::from_hex("1122334411223344112233441122334411223344112233441122334411223344000000000000000000000000112233445566778899001122334455667788990000000000000000000000000000000000000000000000000000000000000004D2").unwrap()))
+        .build_script(&always_success_out_point, Default::default())
         .expect("script");
     let lock_script_dep = CellDep::new_builder()
-        .out_point(out_point)
+        .out_point(always_success_out_point)
+        .build();
+    
+    let contract_bin: Bytes = Loader::default().load_binary("parent-bridge");
+    let contract_out_point = context.deploy_cell(contract_bin);
+    let bridge_script = context
+        .build_script(&contract_out_point, Bytes::from(Vec::from_hex("1122334411223344112233441122334411223344112233441122334411223344000000000000000000000000112233445566778899001122334455667788990000000000000000000000000000000000000000000000000000000000000004D2AAAAAAAA").unwrap()))
+        .expect("script");
+    let bridge_script_dep = CellDep::new_builder()
+        .out_point(contract_out_point)
         .build();
     
 
-    // prepare cells
     let input_out_point = context.create_cell(
         CellOutput::new_builder()
-            .capacity(1000u64.pack())
+            .capacity(10u64.pack())
             .lock(lock_script.clone())
             .build(),
-        Bytes::from(Vec::from_hex("").unwrap()),
+        Default::default(),
     );
     let input = CellInput::new_builder()
         .previous_output(input_out_point)
         .build();
+    
     let outputs = vec![
         CellOutput::new_builder()
-            .capacity(500u64.pack())
+            .capacity(0u64.pack())
             .lock(lock_script.clone())
-            .build(),
-        CellOutput::new_builder()
-            .capacity(500u64.pack())
-            .lock(lock_script)
+            .type_(Some(bridge_script.clone()).pack())
             .build(),
     ];
 
-    let outputs_data = vec![Bytes::new(); 2];
+    let outputs_data = vec![Bytes::new(); 1];
+
+    let witnesses = vec![Bytes::from(vec![0 as u8; 1]); 1];
 
     // build transaction
     let tx = TransactionBuilder::default()
@@ -57,6 +62,8 @@ fn test_basic() {
         .outputs(outputs)
         .outputs_data(outputs_data.pack())
         .cell_dep(lock_script_dep)
+        .cell_dep(bridge_script_dep)
+        .witnesses(witnesses.pack())
         .build();
     let tx = context.complete_tx(tx);
 
