6172ddc0cff477a5a83ec1f8f683c00de5db80b4
Johann Barbie <johannbarbie@me.com>
2020-09-08 19:06:38 +0200

    left comment to explain empty witness

--- packages/parent-bridge/tests/src/tests.rs
+++ packages/parent-bridge/tests/src/tests.rs
@@ -51,6 +51,8 @@ fn test_basic() {
 
     let outputs_data = vec![Bytes::new(); 1];
 
+    // in combat the secp256 lock script would check the withness
+    // for a signature, hence we can't use an actionByte here.
     let witnesses = vec![Bytes::new()];
 
     // build transaction
