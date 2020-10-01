831e5c32031a7642293122009a9232dde3b2f078
Jan <jan@parseclabs.org>
2020-10-01 12:20:58 +0200

    Deposit lock script.

--- packages/parent-bridge/tests/src/tests.rs
+++ packages/parent-bridge/tests/src/tests.rs
@@ -540,3 +540,63 @@ fn test_too_many_type_outputs() {
 
     assert_error_eq!(err, ScriptError::ValidationFailure(11));
 }
+
+fn test_deposit_with_lock() {
+    let mut context = Context::default();
+
+    let always_success_out_point = context.deploy_cell(ALWAYS_SUCCESS.clone());
+    let always_success_script = context
+        .build_script(&always_success_out_point, Default::default())
+        .expect("script");
+    let always_success_script_dep = CellDep::new_builder()
+        .out_point(always_success_out_point)
+        .build();
+
+    let deposit_lock_args : Bytes = Default::default();
+    let depsit_lock_bin: Bytes = Loader::default().load_binary("deposit-lock");
+    let depsit_lock_out_point = context.deploy_cell(depsit_lock_bin);
+    let depsit_lock_script = context
+        .build_script(&depsit_lock_out_point, deposit_lock_args)
+        .expect("script");
+    let depsit_lock_dep = CellDep::new_builder().out_point(depsit_lock_out_point).build();
+
+    // let input_out_point = context.create_cell(
+    //     CellOutput::new_builder()
+    //         .capacity(10u64.pack())
+    //         .lock(lock_script.clone())
+    //         .build(),
+    //     Default::default(),
+    // );
+    // let input = CellInput::new_builder()
+    //     .previous_output(input_out_point)
+    //     .build();
+
+    // let outputs = vec![CellOutput::new_builder()
+    //     .capacity(0u64.pack())
+    //     .lock(lock_script.clone())
+    //     .type_(Some(bridge_script.clone()).pack())
+    //     .build()];
+
+    // let outputs_data = vec![Bytes::new(); 1];
+
+    // // in combat the secp256 lock script would check the withness
+    // // for a signature, hence we can't use an actionByte here.
+    // let witnesses = vec![Bytes::new()];
+
+    // // build transaction
+    // let tx = TransactionBuilder::default()
+    //     .input(input)
+    //     .outputs(outputs)
+    //     .outputs_data(outputs_data.pack())
+    //     .cell_dep(lock_script_dep)
+    //     .cell_dep(bridge_script_dep)
+    //     .witnesses(witnesses.pack())
+    //     .build();
+    // let tx = context.complete_tx(tx);
+
+    // // run
+    // let cycles = context
+    //     .verify_tx(&tx, MAX_CYCLES)
+    //     .expect("pass verification");
+    // println!("consume cycles: {}", cycles);
+}
