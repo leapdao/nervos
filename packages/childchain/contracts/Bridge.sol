44735e3f88988d33acb280de224bf39a87c0ff6d
Mwaa Joseph <mwaaj7@gmail.com>
2020-10-22 15:24:11 +0300

    Fix childchain bridge & tests (#51)
    * failing tests
    
    * Fix childchain contracts and tests
    
    Co-authored-by: Johann Barbie <johannbarbie@me.com>

--- packages/childchain/contracts/Bridge.sol
+++ packages/childchain/contracts/Bridge.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.4.21 <0.7.2;
 pragma experimental ABIEncoderV2;
 
 /**
@@ -40,6 +40,7 @@ contract Bridge {
   event BurnQuorum(bytes32 indexed txHash, address indexed from, uint256 amount, Sig[] signatures);
 
   address[] public validators;
+  
   mapping(bytes32 => mapping(address => bool)) lockSigs;
 
   struct Sig {
@@ -50,7 +51,7 @@ contract Bridge {
 
   struct Receipt {
     bool isLock;
-    address user;
+    address payable user;
     uint256 amount;
     bytes32 txHash;
   }
@@ -84,6 +85,29 @@ contract Bridge {
     return (ret, addr);
   }
 
+  function getUnlockSignatures(bytes32 txHash) internal view returns (Sig[] memory) {
+    Sig[] memory signatures  = new Sig[](validators.length * 2 / 3 + 1);
+    // https://medium.com/codechain/why-n-3f-1-in-the-byzantine-fault-tolerance-system-c3ca6bab8fe9
+    uint256 fillUntil = 0;
+    for (uint256 i = 0; i < validators.length; i++){
+      if (unlockSigs[txHash][validators[i]].v > 0) {
+        signatures[fillUntil] = unlockSigs[txHash][validators[i]];
+        fillUntil++;
+      }
+    }
+    return signatures;
+  }
+  
+  function getValidatorAddress(Receipt memory receipt, Sig memory sig) internal returns (address) {
+    bytes32 receiptHash = keccak256(abi.encode(receipt.isLock, receipt.user, receipt.amount, receipt.txHash));
+    // prefixed hash to mimic the behavior of eth_sign.
+    bytes32 sigHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", receiptHash));
+    
+    address signer;
+    (, signer) = safer_ecrecover(sigHash, sig.v, sig.r, sig.s);
+    return signer;
+  }
+
   function isMintComplete(bytes32 txHash) internal view returns (bool) {
     return lockSigs[txHash][address(0)];
   }
@@ -92,34 +116,13 @@ contract Bridge {
     return unlockSigs[txHash][address(0)].v > 0;
   }
 
-
-  function isValidReceiptSig(Receipt memory receipt, Sig memory sig) internal returns (bool) {
-    bytes32 sigHash = keccak256(abi.encode(receipt.isLock, receipt.user, receipt.amount, receipt.txHash));
-    address signer;
-    (, signer) = safer_ecrecover(sigHash, sig.v, sig.r, sig.s);
-    // what next?
-  }
-
-  /**
-   * @dev Sets the list of validators that are allowed to relay events.
-   */
-  constructor(address[] memory _validators) public {
-    validators = _validators;
-  }
-
-  // getting error:
-  //     Error: invalid tuple value (coderType="tuple", value="0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000003fe6b42cbd2da14465d4b4b672e51e043d7adc6c0000000000000000000000000000000000000000000000001bc16d674ec800007006b92d930641648160ddba292bf0fe55982dda60202c4ba73762e09005a0ee")
-  // when trying to call this with:
-  //     const receipt = unlockReceipt(accounts[0], '2000000000000000000', txHash);
-  //     tx = await bridge.collect(receipt.getAbiReceipt(), receipt.getAbiSig(ALICE_PRIV));
-  // in unit tests
-  function collect(Receipt memory receipt, Sig memory sig) public {
-    require(isValidReceiptSig(receipt, sig), "invalid sig");
-    if (receipt.isLock) {
-      //collectLock(receipt.user, receipt.amount, receipt.txHash, sig.v, sig.r, sig.s);
-    } else {
-      //collectUnlock(receipt.user, receipt.amount, receipt.txHash, sig.v, sig.r, sig.s);
+  function isValidSig(address signer) internal view returns (bool) {
+    for (uint256 i = 0; i < validators.length; i++) {
+      if (validators[i] == signer) {
+        return true;
+      }
     }
+    return false;
   }
 
   /**
@@ -133,48 +136,31 @@ contract Bridge {
     address payable to,
     uint256 amount,
     bytes32 txHash,
-    uint8 v,
-    bytes32 r,
-    bytes32 s) public {
+    address signer) internal {
     // check the lock
-    require(!isMintComplete(txHash), "mint already executed");
-    require(amount > 0, "amount needs to be larger than zero");
-    bytes32 sigHash = keccak256(abi.encode(true, to, amount, txHash));
-    address signer;
-    (, signer) = safer_ecrecover(sigHash, v, r, s);
-    uint256 signerCount = 0;
-    require(!lockSigs[txHash][signer], "signature already collected");
-    for (v = 0; v < validators.length; v++) {
-      // add the new signature
-      if (validators[v] == signer) {
-        // payload
-        lockSigs[txHash][signer] = true;
-        emit LockSig(txHash, signer, to, amount);
-      }
-      // count all available signatures
+    require(!isMintComplete(txHash), "Mint already executed");
+    require(!lockSigs[txHash][signer], "Signature already collected");
+
+    // Add validator signature against transaction hash
+    lockSigs[txHash][signer] = true;
+    emit LockSig(txHash, signer, to, amount);
+
+    // Count all available signatures
+    uint256 signerCount;
+    for (uint256 v = 0; v < validators.length; v++) {
       if (lockSigs[txHash][validators[v]]) {
         signerCount++;
       }
     }
+
     // check for quorum
     if (signerCount > validators.length * 2 / 3) {
       // set the lock
       lockSigs[txHash][address(0)] = true;
       // how to mint?! :shrug:
+      // address(uint160(to)).transfer(amount);
       to.transfer(amount);
       emit Mint(to, amount);
-    }
-    require(lockSigs[txHash][signer] == true, "Signer needs to be part of validator set");
-  }
-
-  /**
-   * @dev moves `msg.value` tokens from `msg.sender` account into non-circulating supply.
-   *
-   * Emits a {Burn} event.
-   */
-  function () payable external {
-    if(msg.sender != deployer){
-      emit Burn(msg.sender, msg.value);
     }
   }
 
@@ -189,48 +175,60 @@ contract Bridge {
     address from,
     uint256 amount,
     bytes32 txHash,
-    uint8 v,
-    bytes32 r,
-    bytes32 s) public {
+    Sig memory sig,
+    address signer) internal {
     // check the unlock
-    require(!isUnlockComplete(txHash), "unlock quorum already reached");
-    require(amount > 0, "amount needs to be larger than zero");
-    require(address(0) != from, "can not receive from zero address");
-    require(bytes32(0) != txHash, "txHash not equal zero");
-    bytes32 sigHash = keccak256(abi.encode(false, from, amount, txHash));
-    address signer;
-    (, signer) = safer_ecrecover(sigHash, v, r, s);
-    uint256 signerCount = 0;
-    require(unlockSigs[txHash][signer].v == 0, "signature already collected");
-    for (uint256 i = 0; i < validators.length; i++) {
-      if (validators[i] == signer) {
-        // payload
-        unlockSigs[txHash][signer] = Sig({
-          v: v,
-          r: r,
-          s: s
-        });
-        emit UnlockSig(txHash, signer, from, amount);
-      }
-      if (unlockSigs[txHash][validators[i]].v > 0) {
+    require(!isUnlockComplete(txHash), "Unlock quorum already reached");
+    require(unlockSigs[txHash][signer].v == 0, "Signature already collected");
+
+    // payload
+    unlockSigs[txHash][signer] = sig;
+    emit UnlockSig(txHash, signer, from, amount);
+
+    // Count all available signatures
+    uint256 signerCount;
+    for (uint256 u = 0; u < validators.length; u++) {
+      if (unlockSigs[txHash][validators[u]].v > 0) {
         signerCount++;
       }
     }
+
     if (signerCount > validators.length * 2 / 3) {
-      // how to mint?! :shrug:
       // set the lock
-      Sig[] memory signatures  = new Sig[](validators.length * 2 / 3 + 1);
-      // https://medium.com/codechain/why-n-3f-1-in-the-byzantine-fault-tolerance-system-c3ca6bab8fe9
-      uint256 fillUntil = 0;
-      for (uint256 i = 0; i < validators.length; i++){
-        if (unlockSigs[txHash][validators[i]].v > 0) {
-          signatures[fillUntil] = unlockSigs[txHash][validators[i]];
-          fillUntil++;
-        }
-      }
       unlockSigs[txHash][address(0)].v = 1;
-      emit BurnQuorum(txHash, from, amount, signatures);
+      emit BurnQuorum(txHash, from, amount, getUnlockSignatures(txHash));
     }
-    require(unlockSigs[txHash][signer].v > 0, "Signer needs to be part of validator set");
+  }
+
+  /**
+   * @dev Sets the list of validators that are allowed to relay events.
+   */
+  constructor(address[] memory _validators) {
+    validators = _validators;
+  }
+
+  function collect(Receipt memory receipt, Sig memory sig) public {
+    require(receipt.amount > 0, "Amount needs to be larger than zero");
+    require(bytes32(0) != receipt.txHash, "txHash not equal zero");
+    require(address(0) != receipt.user, "Can not receive from zero address");
+    address signer = getValidatorAddress(receipt, sig); // Get validator address
+    require(isValidSig(signer), "Invalid: Signature needs to be part of validator set");
+
+    if (receipt.isLock) {
+      collectLock(receipt.user, receipt.amount, receipt.txHash, signer);
+    } else {
+      collectUnlock(receipt.user, receipt.amount, receipt.txHash, sig, signer);
+    }
+  }
+
+  /**
+   * @dev On contract receiving tokens from msg.sender. 
+   *
+   * Emits a {Burn} event.
+   */
+  receive () payable external {
+    if(msg.sender != deployer){
+      emit Burn(msg.sender, msg.value);
+    }
   }
 }
